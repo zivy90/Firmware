91080ea2df85d57bd76083605fc54fc7d18a2a00
Roman <bapstroman@gmail.com>
2018-03-14 15:03:28 +0100

    cmake configs: don't build wind estimator for fmu-v2 due to lack of flash space
    Signed-off-by: Roman <bapstroman@gmail.com>

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -117,7 +117,7 @@ set(config_module_list
 	#modules/local_position_estimator
 	#modules/position_estimator_inav
 	#modules/landing_target_estimator
-	modules/wind_estimator
+	#modules/wind_estimator
 
 	#
 	# Vehicle Control
