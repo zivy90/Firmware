4db1ce3b4138b234f62b8bc7b99ca852fdafc773
TSC21 <n.marques21@hotmail.com>
2018-09-15 10:07:54 +0100

    setup: osx: fix pip install of pyyaml

--- Tools/setup/OSX.sh
+++ Tools/setup/OSX.sh
@@ -6,9 +6,13 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 brew tap PX4/px4
 brew install px4-dev
 
-# python dependencies
-sudo easy_install pip
-sudo -H pip install -r ${DIR}/requirements.txt
-
 # Optional, but recommended additional simulation tools:
 brew install px4-sim
+
+# python dependencies
+python -m pip install --upgrade pip setuptools
+python -m pip install -r ${DIR}/requirements.txt
+
+export PATH=/Library/Python/2.7/bin:$PATH
+export PATH=/Library/Python/2.7/site-packages:$PATH
+export MANPATH=/Library/Python/2.7/share/man:$MANPATH
